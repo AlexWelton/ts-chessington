ea3bd62316f15db1d4821bc745614c407c3ebbbb
Bianca <bianca.darolti@softwire.com>
2022-09-30 15:09:56 +0100

    King moves

--- src/engine/pieces/king.ts
+++ src/engine/pieces/king.ts
@@ -1,6 +1,7 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from "../square";
 
 export default class King extends Piece {
     public constructor(player: Player) {
@@ -8,6 +9,15 @@ export default class King extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        let moves = new Array(0);
+        let location = board.findPiece(this);
+
+        for (let xOffset of [-1,0,1]) {
+            for (let yOffset of [-1,0,1]) {
+                if(! (xOffset == 0 && yOffset == 0)) moves.push(Square.at(location.row+xOffset, location.col+yOffset));
+            }
+        }
+
+        return moves;
     }
 }
