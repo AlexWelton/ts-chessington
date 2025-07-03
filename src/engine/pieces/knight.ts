0dc43131e44ec31fb72121f9ee84d6de2a9f7a0a
Alex Welton <Alex.Welton@softwire.com>
2025-07-03 11:59:31 +0100

    Knight moves

--- src/engine/pieces/knight.ts
+++ src/engine/pieces/knight.ts
@@ -1,6 +1,7 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from "../square";
 
 export default class Knight extends Piece {
     public constructor(player: Player) {
@@ -8,6 +9,16 @@ export default class Knight extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        let moves = new Array(0);
+        let location = board.findPiece(this);
+
+        for (let direction of [-1,1]) {
+            for (let orientation of [-1,1]) {
+                moves.push(Square.at(location.row + direction*2, location.col + orientation));
+                moves.push(Square.at(location.col + orientation, location.col + direction*2));
+            }
+        }
+
+        return moves;
     }
 }
