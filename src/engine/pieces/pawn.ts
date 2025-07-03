4daaa62432af85906bf22b0552410fd6df2e2786
Alex Welton <Alex.Welton@softwire.com>
2025-07-03 09:59:58 +0100

    Pawn double movement

--- src/engine/pieces/pawn.ts
+++ src/engine/pieces/pawn.ts
@@ -13,8 +13,13 @@ export default class Pawn extends Piece {
         let moves = new Array(0);
 
         let direction = this.player == Player.WHITE ? 1 : -1;
+        let homerow = this.player == Player.WHITE ? 1 : 6;
 
+        //Single move
         moves.push(Square.at(location.row+direction,location.col));
+
+        //Double move
+        if (location.row == homerow) moves.push(Square.at(location.row+(direction*2),location.col));
 
         return moves;
     }
