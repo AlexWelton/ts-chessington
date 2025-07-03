47d1c749df11a260e9fde502afacd3de237bcc3f
Alex Welton <Alex.Welton@softwire.com>
2025-07-03 10:28:28 +0100

    Bishop movement

--- src/engine/pieces/bishop.ts
+++ src/engine/pieces/bishop.ts
@@ -1,6 +1,7 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from "../square";
 
 export default class Bishop extends Piece {
     public constructor(player: Player) {
@@ -8,6 +9,20 @@ export default class Bishop extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        let moves = new Array(0);
+        let location = board.findPiece(this);
+
+        let diff = Math.abs(location.col-location.row);
+        for (let row = 0, col = row + diff; row < 8 && col < 8; row++, col = row + diff) {
+            if (!location.equals(Square.at(row,col))) moves.push(Square.at(row, col));
+        }
+
+        let sum = location.col+location.row;
+        for (let row = 0, col = sum - row; row < 8 && col >= 0 && col < 8; row++, col = sum - row) {
+
+            if (!location.equals(Square.at(row,col))) moves.push(Square.at(row, col));
+        }
+
+        return moves;
     }
 }
